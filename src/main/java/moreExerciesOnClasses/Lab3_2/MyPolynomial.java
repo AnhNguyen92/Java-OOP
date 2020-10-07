60a739e3ee33545f8f10bec66a85784e2de47009
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-07 08:53:51 +0700

    update code

--- src/main/java/moreExerciesOnClasses/Lab3_2/MyPolynomial.java
+++ src/main/java/moreExerciesOnClasses/Lab3_2/MyPolynomial.java
@@ -13,50 +13,47 @@ public class MyPolynomial {
 	public int getDegree() {
 		return coeffs.length - 1;
 	}
-	
+
 	public double evaluate(double x) {
 		double evaluate = 0.0;
 		for (int i = 0; i < coeffs.length; i++) {
 			evaluate += coeffs[i] * Math.pow(x, i);
 		}
-		
+
 		return evaluate;
 	}
-	
+
 	public MyPolynomial add(MyPolynomial right) {
 		int maxDegree = Math.max(this.getDegree(), right.getDegree());
 		double[] coeffsResult = new double[maxDegree];
-		
-//		for (int i = 0; i < coeffs.length; i++) {
-//			coeffsResult[i] = coeffs[i];
-//		}
-		coeffsResult = Arrays.copyOf(coeffs, coeffs.length);
-		
+
+		System.arraycopy(coeffs, 0, coeffsResult, 0, coeffs.length);
+
 		for (int i = 0; i < right.coeffs.length; i++) {
 			coeffsResult[i] += right.coeffs[i];
 		}
 		this.coeffs = coeffsResult;
-		
+
 		return this;
 	}
-	
+
 	public MyPolynomial multiply(MyPolynomial right) {
 		int maxDegree = this.coeffs.length + right.coeffs.length;
 		double[] coeffsResult = new double[maxDegree];
 		for (int i = 0; i < coeffsResult.length; i++) {
 			coeffsResult[i] = 0.0;
 		}
-		
+
 		for (int i = 0; i < this.getDegree(); i++) {
 			for (int j = 0; j < right.getDegree(); j++) {
 				coeffsResult[i + j] += coeffs[i] * right.coeffs[j];
 			}
 		}
 		this.coeffs = coeffsResult;
-		
+
 		return this;
 	}
-	
+
 	@Override
 	public String toString() {
 		StringBuilder builder = new StringBuilder();
@@ -66,10 +63,9 @@ public class MyPolynomial {
 				builder.append("+");
 			}
 		}
-		builder.append(coeffs[0]);		
-		
+		builder.append(coeffs[0]);
+
 		return "MyPolynomial [coeffs=" + Arrays.toString(coeffs) + "]";
 	}
-	
-	
+
 }
