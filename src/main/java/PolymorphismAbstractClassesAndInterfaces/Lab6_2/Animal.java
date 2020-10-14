25e0bc20f29a77216b909834cbe8b67388cedbe2
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 16:05:26 +0700

    lab 6.2

---
+++ src/main/java/PolymorphismAbstractClassesAndInterfaces/Lab6_2/Animal.java
@@ -0,0 +1,5 @@
+package main.java.PolymorphismAbstractClassesAndInterfaces.Lab6_2;
+
+abstract public class Animal {
+	abstract public void greeting();
+}
---
+++ src/main/java/PolymorphismAbstractClassesAndInterfaces/Lab6_2/Cat.java
@@ -0,0 +1,8 @@
+package main.java.PolymorphismAbstractClassesAndInterfaces.Lab6_2;
+
+public class Cat extends Animal {
+	@Override
+	public void greeting() {
+		System.out.println("Meow!");
+	}
+}
