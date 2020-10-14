2358e60ea1ef7a452f1cc432323bff8ebf630173
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 19:38:31 +0700

    lab 4.5

--- src/main/java/Inheritance/Lab4_5/Circle.java
+++ src/main/java/Inheritance/Lab4_5/Circle.java
@@ -1,5 +1,35 @@
 package main.java.Inheritance.Lab4_5;
 
-public class Circle {
+public class Circle extends Shape {
+	private double radius = 1.0;
+
+	public Circle() {
+		super();
+	}
+
+	public Circle(String color, boolean filled) {
+		super(color, filled);
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public double getArea() {
+		return Math.PI * radius * radius;
+	}
+
+	public double getPerimeter() {
+		return Math.PI * (radius + radius);
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + "]";
+	}
 
 }
