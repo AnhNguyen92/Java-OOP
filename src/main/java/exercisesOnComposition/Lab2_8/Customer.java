0f116797971cfcedaa80aa571cb65e8a5177ab1e
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 11:17:20 +0700

    lab 2.8

--- src/main/java/exercisesOnComposition/Lab2_8/Customer.java
+++ src/main/java/exercisesOnComposition/Lab2_8/Customer.java
@@ -4,6 +4,25 @@ public class Customer {
 private int ID;
 private String name;
 private char gender;
+public Customer(int iD, String name, char gender) {
+	super();
+	ID = iD;
+	this.name = name;
+	this.gender = gender;
+}
+public int getID() {
+	return ID;
+}
+public String getName() {
+	return name;
+}
+public char getGender() {
+	return gender;
+}
+@Override
+public String toString() {
+	return "Customer [ID=" + ID + ", name=" + name + ", gender=" + gender + "]";
+}
 
 
 }
