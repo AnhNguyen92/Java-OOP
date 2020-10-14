346a17e727aa54f974d490f2ec1787650dfec4b5
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 20:28:35 +0700

    lab 3.6

--- src/main/java/ClassesMore/Lab3_6/Ball.java
+++ src/main/java/ClassesMore/Lab3_6/Ball.java
@@ -1,5 +1,77 @@
 package main.java.ClassesMore.Lab3_6;
 
 public class Ball {
+	private float x;
+	private float y;
+	private int radius;
+	private float xDelta;
+	private float yDelta;
+
+	public Ball(float x, float y, int radius, float xDelta, float yDelta) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.radius = radius;
+		this.xDelta = xDelta;
+		this.yDelta = yDelta;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public void setX(float x) {
+		this.x = x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public void setY(float y) {
+		this.y = y;
+	}
+
+	public int getRadius() {
+		return radius;
+	}
+
+	public void setRadius(int radius) {
+		this.radius = radius;
+	}
+
+	public float getxDelta() {
+		return xDelta;
+	}
+
+	public void setxDelta(float xDelta) {
+		this.xDelta = xDelta;
+	}
+
+	public float getyDelta() {
+		return yDelta;
+	}
+
+	public void setyDelta(float yDelta) {
+		this.yDelta = yDelta;
+	}
+
+	public void move() {
+		x += xDelta;
+		y += yDelta;
+	}
+
+	public void reflectHorizontal() {
+		xDelta *= -1;
+	}
+
+	public void reflectVertical() {
+		yDelta *= -1;
+	}
+
+	@Override
+	public String toString() {
+		return "Ball [x=" + x + ", y=" + y + ", radius=" + radius + ", xDelta=" + xDelta + ", yDelta=" + yDelta + "]";
+	}
 
 }
