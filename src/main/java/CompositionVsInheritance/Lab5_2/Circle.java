2ff714d948b99e7c4f4085ac76fb991aab92570c
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 17:38:11 +0700

    lab 5.2

--- src/main/java/CompositionVsInheritance/Lab5_2/Circle.java
+++ src/main/java/CompositionVsInheritance/Lab5_2/Circle.java
@@ -1,5 +1,37 @@
 package main.java.CompositionVsInheritance.Lab5_2;
 
 public class Circle {
+	private double radius;
+	private String color;
+
+	public Circle() {
+	}
+
+	public Circle(double radius, String color) {
+		super();
+		this.radius = radius;
+		this.color = color;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	@Override
+	public String toString() {
+		return "Circle [radius=" + radius + ", color=" + color + "]";
+	}
 
 }
--- src/main/java/CompositionVsInheritance/Lab5_2/Cylinder.java
+++ src/main/java/CompositionVsInheritance/Lab5_2/Cylinder.java
@@ -1,5 +1,28 @@
 package main.java.CompositionVsInheritance.Lab5_2;
 
 public class Cylinder {
+	private Circle base;
+	private double height;
+
+	public Cylinder() {
+		base = new Circle(); // Call the constructor to construct the Circle
+		height = 1.0;
+	}
+
+	public Circle getBase() {
+		return base;
+	}
+
+	public void setBase(Circle base) {
+		this.base = base;
+	}
+
+	public double getHeight() {
+		return height;
+	}
+
+	public void setHeight(double height) {
+		this.height = height;
+	}
 
 }
