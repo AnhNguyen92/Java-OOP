e4516239617d650ba52769172d098cbd13be606a
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 10:58:04 +0700

    lab2.7

---
+++ src/main/java/exercisesOnComposition/Lab2_7/Customer.java
@@ -0,0 +1,35 @@
+package main.java.exercisesOnComposition.Lab2_7;
+
+public class Customer {
+	private int ID;
+	private String name;
+	private int discount;
+
+	public Customer(int iD, String name, int discount) {
+		ID = iD;
+		this.name = name;
+		this.discount = discount;
+	}
+
+	public int getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(int discount) {
+		this.discount = discount;
+	}
+
+	public int getID() {
+		return ID;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [ID=" + ID + ", name=" + name + ", discount=" + discount + "]";
+	}
+
+}
