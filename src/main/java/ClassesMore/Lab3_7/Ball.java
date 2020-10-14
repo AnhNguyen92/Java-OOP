c8bb13d20d4f604a51f622035f6b2c074c13fa29
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 20:47:47 +0700

    lab 3.7

--- src/main/java/ClassesMore/Lab3_7/Ball.java
+++ src/main/java/ClassesMore/Lab3_7/Ball.java
@@ -1,5 +1,38 @@
 package main.java.ClassesMore.Lab3_7;
 
 public class Ball {
+	private float x;
+	private float y;
+	private float z;
+
+	public Ball(float x, float y, float z) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public float getX() {
+		return x;
+	}
+
+	public float getY() {
+		return y;
+	}
+
+	public float getZ() {
+		return z;
+	}
+
+	public void setXYZ(float x, float y, float z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	@Override
+	public String toString() {
+		return "Ball [x=" + x + ", y=" + y + ", z=" + z + "]";
+	}
 
 }
--- src/main/java/ClassesMore/Lab3_7/Player.java
+++ src/main/java/ClassesMore/Lab3_7/Player.java
@@ -1,5 +1,36 @@
 package main.java.ClassesMore.Lab3_7;
 
 public class Player {
+	private int number;
+	private float x;
+	private float y;
+	private float z = 0.0f;
 
+	public Player(int number, float x, float y, float z) {
+		super();
+		this.number = number;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void move(float xDisp, float yDisp, float zDisp) {
+		this.x = xDisp;
+		this.y = yDisp;
+		this.z = zDisp;
+	}
+
+	public void jump(float zDisp) {
+		this.z = zDisp;
+	}
+
+	public boolean near(Ball ball) {
+		double distance = Math.sqrt((x - ball.getX()) * (x - ball.getX()) + (y - ball.getY()) * (y - ball.getY())
+				+ (z - ball.getZ()) * (z - ball.getZ()));
+		return distance < 8;
+	}
+
+	public void kick(Ball ball) {
+		ball.setXYZ(ball.getX() - 2, ball.getY() - 2, 0);
+	}
 }
