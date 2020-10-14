ec30f9903ff70e356bdbd7de16b1ea36e823f42d
anhnguyenids <anh.nguyen@idsolutions.com.vn>
2020-10-14 16:48:24 +0700

    lab 7.1

--- src/main/java/OOPMore/Lab7_1/DiscountRate.java
+++ src/main/java/OOPMore/Lab7_1/DiscountRate.java
@@ -1,5 +1,43 @@
 package main.java.OOPMore.Lab7_1;
 
 public class DiscountRate {
+	private double serviceDiscountPremium = 0.2;
+	private double serviceDiscountGold = 0.15;
+	private double serviceDiscountSilver = 0.1;
+	private double productDiscountPremium = 0.1;
+	private double productDiscountGold = 0.1;
+	private double productDiscountSilver = 0.1;
+
+	public double getServiceDiscountRate(String type) {
+		double discountRate = serviceDiscountSilver;
+		switch (type) {
+		case "Premium":
+			discountRate = serviceDiscountPremium;
+			break;
+		case "Gold":
+			discountRate = serviceDiscountGold;
+			break;
+		default: // Silver
+			break;
+		}
+
+		return discountRate;
+	}
+
+	public double getProductDiscountRate(String type) {
+		double discountRate = productDiscountSilver;
+		switch (type) {
+		case "Premium":
+			discountRate = productDiscountPremium;
+			break;
+		case "Gold":
+			discountRate = productDiscountGold;
+			break;
+		default: // Silver
+			break;
+		}
+
+		return discountRate;
+	}
 
 }
--- src/main/java/OOPMore/Lab7_1/Visit.java
+++ src/main/java/OOPMore/Lab7_1/Visit.java
@@ -1,5 +1,47 @@
 package main.java.OOPMore.Lab7_1;
 
+import java.util.Date;
+
 public class Visit {
+	private Customer customer;
+	private Date date;
+	private double serviceExpense;
+	private double productExpense;
+
+	public Visit(String name, Date date) {
+		Customer cust = new Customer(name);
+		this.customer = cust;
+		this.date = date;
+	}
+
+	public String getName() {
+		return customer.getName();
+	}
+
+	public double getServiceExpense() {
+		return serviceExpense;
+	}
+
+	public void setServiceExpense(double serviceExpense) {
+		this.serviceExpense = serviceExpense;
+	}
+
+	public double getProductExpense() {
+		return productExpense;
+	}
+
+	public void setProductExpense(double productExpense) {
+		this.productExpense = productExpense;
+	}
+
+	public double getToltalExpense() {
+		return serviceExpense + productExpense;
+	}
+
+	@Override
+	public String toString() {
+		return "Visit [customer=" + customer + ", date=" + date + ", serviceExpense=" + serviceExpense
+				+ ", productExpense=" + productExpense + "]";
+	}
 
 }
